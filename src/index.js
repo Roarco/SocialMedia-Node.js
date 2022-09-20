5001fab60a7af82e0e9011aa12ce203ccc8dcadf
Roarco <sebuscaroberth@gmail.com>
2022-09-19 21:20:36 -0500

    Documentando la API con Swagger

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const app = express();
 const config = require('./config');
 const routes = require('./network/routes');
 const port = config.port;
+const swagger = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 app.use(express.json());
+app.use('/api-docs', swagger.serve, swagger.setup(swaggerDocument));
 
 //Router set
 routes(app);
